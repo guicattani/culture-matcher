390289fd8c9fa1ca8690cc6a57d7065c635c277b
Guilherme Cattani <gcattani1@gmail.com>
2023-07-20 01:41:11 +0200

    Improve culture types table

--- db/migrate/20230719215546_create_culture_types.rb
+++ db/migrate/20230719215546_create_culture_types.rb
@@ -3,9 +3,18 @@
 class CreateCultureTypes < ActiveRecord::Migration[7.0]
   def change
     create_table :culture_types do |t|
-      t.string :name, null: false
+      t.string :name, null: false, index: { unique: true, name: 'unique_indexed_names' }
 
       t.timestamps
+      
+    end
+
+    reversible do |direction|
+      direction.up do
+        execute <<-SQL
+         INSERT INTO culture_types VALUES (1, 'undefined', NOW(), NOW());
+        SQL
+      end
     end
   end
 end
