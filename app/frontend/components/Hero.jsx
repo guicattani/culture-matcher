f5c9e4cd0f8e34965d3ce10b90f12d6e2475801e
Guilherme Cattani <gcattani1@gmail.com>
2023-07-23 23:29:56 +0200

    Remove unused components and logo

--- app/frontend/components/Hero.jsx
+++ app/frontend/components/Hero.jsx
@@ -1,10 +1,10 @@
-import Version from '~/components/Version'
-
-export default function Hero () {
-  return <>
-    <h1 className="hero">
-      <span className="vite smooth">Vite</span> <span className="rails smooth">Rails</span>
-    </h1>
-    <Version/>
-  </>
+export default function Hero() {
+  return (
+    <>
+      <h1 className="hero">
+        <span className="vite smooth">Culture</span>{" "}
+        <span className="rails smooth">Matcher</span>
+      </h1>
+    </>
+  );
 }
